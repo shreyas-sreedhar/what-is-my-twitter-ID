f6bacfd839033be5e051369f058af9ba00153e05
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2024-08-26 19:35:21 -0400

    wip

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('frontend'))
 const btokn = process.env.BEARER_TOKEN;
